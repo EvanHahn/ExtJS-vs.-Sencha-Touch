b32e0088e94dcd81c1bab3f953c1f6c61d963173
Evan Hahn <me@evanhahn.com>
2013-05-17 15:21:53 -0700

    Show a first bit of the data

---
+++ data.js
@@ -0,0 +1,47 @@
+var data = [
+	{
+		"name": "Ext._startTime",
+		"ext": true
+	},
+	{
+		"name": "$previous",
+		"ext": true
+	},
+	{
+		"name": "Function.prototype.$extIsFunction",
+		"ext": true
+	},
+	{
+		"name": "Ext.enumerables",
+		"ext": "http://docs.sencha.com/extjs/4.2.0/#!/api/Ext-property-enumerables",
+		"touch": "http://docs.sencha.com/touch/2.2.0/#!/api/Ext-property-enumerables"
+	},
+	{
+		"name": "Ext.apply",
+		"ext": "http://docs.sencha.com/extjs/4.2.0/#!/api/Ext-method-apply",
+		"touch": "http://docs.sencha.com/touch/2.2.0/#!/api/Ext-method-apply"
+	},
+	{
+		"name": "Ext.buildSettings",
+		"ext": true,
+		"touch": true,
+		"different": true
+	},
+	{
+		"name": "Ext.name",
+		"ext": "http://docs.sencha.com/extjs/4.2.0/#!/api/Ext-property-name"
+	},
+	{
+		"name": "Ext.emptyFn",
+		"ext": "http://docs.sencha.com/extjs/4.2.0/#!/api/Ext-property-emptyFn",
+		"touch": "http://docs.sencha.com/touch/2.2.0/#!/api/Ext-property-emptyFn"
+	},
+	{
+		"name": "Ext.identityFn",
+		"ext": "http://docs.sencha.com/extjs/4.2.0/#!/api/Ext-method-identityFn"
+	},
+	{
+		"name": "Ext.emptyString",
+		"ext": "http://docs.sencha.com/extjs/4.2.0/#!/api/Ext-property-emptyString"
+	}
+];
